3ee72c501d636587a48b4972b5e31e952e30d16f
ks606 <ks606@yandex.ru>
2023-10-25 12:40:16 +0300

    [ref]: .v, .sv sources

--- sim/src/PRBS_TB.sv
+++ sim/src/PRBS_TB.sv
@@ -2,217 +2,214 @@
 //`include "PRBS_GEN.v"
 //`include "PRBS_RX.v"
 module PRBS_TB;
-	
-	parameter 			PRBS_TYPE = 7;
-	parameter 			BIT_CNT_WIDTH = 10;
-	parameter			ERR_CNT_WIDTH = 10;
-//	parameter 		FIFO_BITS = 1;
-//	parameter 		FIFO_SIZE = 1024;
-	
-	logic 				sys_clk = 0;
-	logic 				sys_rst = 0;
-	
-	logic 				tx_clk = 0;
-	logic 				tx_rst = 0;
-	logic 				tx_prbs_en = 0;		
-	logic 				tx_dout_vld;
-	logic 				tx_dout;
-	
-	logic 				rx1_clk = 0;
-	logic 				rx1_rst = 0;	
-	logic 				rx1_dout_vld;
-	logic 				rx1_dout;
-	
-	logic 				rx2_clk = 0;
-	logic 				rx2_rst = 0;
-	logic 				rx2_dout_vld;
-	logic 				rx2_dout;
-	
-	PRBS_TX #(
+    parameter PRBS_TYPE = 7;
+    parameter BIT_CNT_WIDTH = 10;
+    parameter ERR_CNT_WIDTH = 10;
+//	parameter FIFO_BITS = 1;
+//	parameter FIFO_SIZE = 1024;
+    
+    logic sys_clk = 0;
+    logic sys_rst = 0;
+    
+    logic tx_clk = 0;
+    logic tx_rst = 0;
+    logic tx_prbs_en = 0;		
+    logic tx_dout_vld;
+    logic tx_dout;
+    
+    logic rx1_clk = 0;
+    logic rx1_rst = 0;	
+    logic rx1_dout_vld;
+    logic rx1_dout;
+    
+    logic rx2_clk = 0;
+    logic rx2_rst = 0;
+    logic rx2_dout_vld;
+    logic rx2_dout;
+    
+    PRBS_TX #(
         .PRBS_TYPE(PRBS_TYPE)
     )_PRBS_TX(
-        .clk			(tx_clk), 
-		.rst			(tx_rst),
-		
-		// Input
-		.prbs_en		(tx_prbs_en), 
-		
-        // Output
-		.dout_vld		(tx_dout_vld),
-		.dout			(tx_dout)
-	);
-	
-	logic 				FIFO1_WR_CLK;
-	logic 				FIFO1_RD_CLK;
-	logic 				FIFO1_RST;
-	logic 				FIFO1_WR_EN;
-	logic 				FIFO1_RD_EN;
-	logic 				FIFO1_DIN;
-	logic 				FIFO1_DOUT_VLD;
-	logic [7:0] 		FIFO1_DOUT;
-	logic 				FIFO1_EMPTY;
-	logic 				FIFO1_FULL;
-	logic 				FIFO1_WR_ACK;
-	logic [9:0]			FIFO1_WR_CNT;
-	logic [9:0]			FIFO1_RD_CNT;
-	
-	assign FIFO1_WR_CLK = tx_clk;
-	assign FIFO1_RD_CLK = rx1_clk;
-	assign FIFO1_RST = sys_rst;
-	assign FIFO1_WR_EN = tx_dout_vld;
-	assign FIFO1_RD_EN = 1'b1;
-	assign FIFO1_DIN = tx_dout;
-	
-	FIFO_2CLK
-	_FIFO1(
-		.rst			(!FIFO1_RST),
-		
-		// write section
-		.wr_clk			(FIFO1_WR_CLK),
-		.wr_en			(FIFO1_WR_EN),
-		.din			({FIFO1_DIN, 7'b0000_000}),
-		
-		// read section
-		.rd_clk			(FIFO1_RD_CLK),
-		.rd_en			(FIFO1_RD_EN),
-		.valid			(FIFO1_DOUT_VLD),
-		.dout			(FIFO1_DOUT),
-		
-		.wr_ack			(FIFO1_WR_ACK),
-		.full			(FIFO1_FULL),
-		.empty			(FIFO1_EMPTY),
-		
-		.wr_data_count	(FIFO1_WR_CNT),
-		.rd_data_count	(FIFO1_RD_CNT)
-	);
-	
-	logic 				FIFO2_WR_CLK;
-	logic 				FIFO2_RD_CLK;
-	logic 				FIFO2_RST;
-	logic 				FIFO2_WR_EN;
-	logic 				FIFO2_RD_EN;
-	logic 				FIFO2_DIN;
-	logic 				FIFO2_DOUT_VLD;
-	logic [7:0] 		FIFO2_DOUT;
-	logic 				FIFO2_EMPTY;
-	logic 				FIFO2_FULL;
-	logic 				FIFO2_WR_ACK;
-	logic [9:0]			FIFO2_WR_CNT;
-	logic [9:0]			FIFO2_RD_CNT;
-	
-	assign FIFO2_RST = sys_rst;
-	assign FIFO2_WR_CLK = tx_clk;
-	assign FIFO2_RD_CLK = rx2_clk;
-	assign FIFO2_WR_EN = tx_dout_vld;
-	assign FIFO2_RD_EN = 1'b1;
-	assign FIFO2_DIN = tx_dout;
-	
-	FIFO_2CLK
-	_FIFO2(
-		.rst			(!FIFO2_RST),
-		
-		// write section
-		.wr_clk			(FIFO2_WR_CLK),
-		.wr_en			(FIFO2_WR_EN),
-		.din			({FIFO2_DIN, 7'b0000_000}),
-		
-		// read section
-		.rd_clk			(FIFO2_RD_CLK),
-		.rd_en			(FIFO2_RD_EN),
-		.valid			(FIFO2_DOUT_VLD),
-		.dout			(FIFO2_DOUT),
-		
-		.wr_ack			(FIFO2_WR_ACK),
-		.full			(FIFO2_FULL),
-		.empty			(FIFO2_EMPTY),
+        .clk        (tx_clk), 
+        .rst        (tx_rst),
+        
+        // Inputs
+        .prbs_en    (tx_prbs_en), 
+        
+        // Outputs
+        .dout_vld   (tx_dout_vld),
+        .dout       (tx_dout)
+    );
+    
+    logic       FIFO1_WR_CLK;
+    logic       FIFO1_RD_CLK;
+    logic       FIFO1_RST;
+    logic       FIFO1_WR_EN;
+    logic       FIFO1_RD_EN;
+    logic       FIFO1_DIN;
+    logic       FIFO1_DOUT_VLD;
+    logic [7:0] FIFO1_DOUT;
+    logic       FIFO1_EMPTY;
+    logic       FIFO1_FULL;
+    logic       FIFO1_WR_ACK;
+    logic [9:0] FIFO1_WR_CNT;
+    logic [9:0]	FIFO1_RD_CNT;
+    
+    assign FIFO1_WR_CLK = tx_clk;
+    assign FIFO1_RD_CLK = rx1_clk;
+    assign FIFO1_RST    = sys_rst;
+    assign FIFO1_WR_EN  = tx_dout_vld;
+    assign FIFO1_RD_EN  = 1'b1;
+    assign FIFO1_DIN    = tx_dout;
+    
+    FIFO_2CLK
+    _FIFO1(
+        .rst            (!FIFO1_RST),
+        
+        // write
+        .wr_clk	        (FIFO1_WR_CLK),
+        .wr_en          (FIFO1_WR_EN),
+        .din            ({FIFO1_DIN, 7'b0000_000}),
+        
+        // read
+        .rd_clk         (FIFO1_RD_CLK),
+        .rd_en          (FIFO1_RD_EN),
+        .valid          (FIFO1_DOUT_VLD),
+        .dout           (FIFO1_DOUT),
+        
+        .wr_ack         (FIFO1_WR_ACK),
+        .full           (FIFO1_FULL),
+        .empty          (FIFO1_EMPTY),
+        
+        .wr_data_count  (FIFO1_WR_CNT),
+        .rd_data_count  (FIFO1_RD_CNT)
+    );
+    
+    logic       FIFO2_WR_CLK;
+    logic       FIFO2_RD_CLK;
+    logic       FIFO2_RST;
+    logic       FIFO2_WR_EN;
+    logic       FIFO2_RD_EN;
+    logic       FIFO2_DIN;
+    logic       FIFO2_DOUT_VLD;
+    logic [7:0] FIFO2_DOUT;
+    logic       FIFO2_EMPTY;
+    logic       FIFO2_FULL;
+    logic       FIFO2_WR_ACK;
+    logic [9:0]	FIFO2_WR_CNT;
+    logic [9:0]	FIFO2_RD_CNT;
+    
+    assign FIFO2_RST    = sys_rst;
+    assign FIFO2_WR_CLK = tx_clk;
+    assign FIFO2_RD_CLK = rx2_clk;
+    assign FIFO2_WR_EN  = tx_dout_vld;
+    assign FIFO2_RD_EN  = 1'b1;
+    assign FIFO2_DIN    = tx_dout;
+    
+    FIFO_2CLK
+    _FIFO2(
+        .rst            (!FIFO2_RST),
+        
+        // write section
+        .wr_clk         (FIFO2_WR_CLK),
+        .wr_en	        (FIFO2_WR_EN),
+        .din            ({FIFO2_DIN, 7'b0000_000}),
+        
+        // read section
+        .rd_clk         (FIFO2_RD_CLK),
+        .rd_en          (FIFO2_RD_EN),
+        .valid          (FIFO2_DOUT_VLD),
+        .dout           (FIFO2_DOUT),
+        
+        .wr_ack	        (FIFO2_WR_ACK),
+        .full           (FIFO2_FULL),
+        .empty          (FIFO2_EMPTY),
 
-		.wr_data_count	(FIFO2_WR_CNT),
-		.rd_data_count	(FIFO2_RD_CNT)
-	);	
-	
-	logic 						RX1_BIT_CNT_FULL;
-	logic [BIT_CNT_WIDTH-1:0] 	RX1_BIT_CNT;
-	logic [ERR_CNT_WIDTH-1:0]	RX1_ERR_CNT;
-	logic [9:0]					RX1_SYNC_CNT;
-	logic						RX1_DIN_VLD;
-	logic						RX1_DIN;
-	logic						RX1_DOUT_XOR;
-	
-	assign RX1_DIN_VLD = FIFO1_DOUT_VLD;
-	assign RX1_DIN = FIFO1_DOUT[7];
-	
-	PRBS_RX #(
-		.PRBS_TYPE		(PRBS_TYPE),
-		.BIT_CNT_WIDTH	(BIT_CNT_WIDTH),
-		.ERR_CNT_WIDTH	(ERR_CNT_WIDTH)
-	)_PRBS_RX1(
-		.clk			(rx1_clk),
-		.rst			(rx1_rst),
-		
-		// Input
-		.din_vld		(RX1_DIN_VLD),
-		.din			(RX1_DIN),
-	
-		// Output
-		.bit_cnt		(RX1_BIT_CNT),
-		.bit_cnt_full	(RX1_BIT_CNT_FULL),
-		.err_cnt		(RX1_ERR_CNT),
-		.dout_vld		(rx1_dout_vld),
-		.dout			(rx1_dout),
-		
-		.sync_cnt		(RX1_SYNC_CNT),
-		.dout_xor		(RX1_DOUT_XOR)
-	);
-	
-	logic 						RX2_BIT_CNT_FULL;
-	logic [BIT_CNT_WIDTH-1:0] 	RX2_BIT_CNT;
-	logic [ERR_CNT_WIDTH-1:0]	RX2_ERR_CNT;
-	logic [9:0]					RX2_SYNC_CNT;
-	logic						RX2_DIN_VLD;
-	logic						RX2_DIN;
-	logic						RX2_DOUT_XOR;
-	
-	assign RX2_DIN_VLD = FIFO2_DOUT_VLD;
-	assign RX2_DIN = FIFO2_DOUT[7];
-	
-	PRBS_RX #(
-		.PRBS_TYPE		(PRBS_TYPE),
-		.BIT_CNT_WIDTH	(BIT_CNT_WIDTH),
-		.ERR_CNT_WIDTH	(ERR_CNT_WIDTH)
-	)_PRBS_RX2(
-		.clk			(rx2_clk),
-		.rst			(rx2_rst),
-		
-		// Input
-		.din_vld		(RX2_DIN_VLD),
-		.din			(RX2_DIN),
-	
-		// Output
-		.bit_cnt		(RX2_BIT_CNT),
-		.bit_cnt_full	(RX2_BIT_CNT_FULL),
-		.err_cnt		(RX2_ERR_CNT),
-		.dout_vld		(rx2_dout_vld),
-		.dout			(rx2_dout),
-		
-		.sync_cnt		(RX2_SYNC_CNT),
-		.dout_xor		(RX2_DOUT_XOR)
-	);
-	
-	always #20 sys_clk = !sys_clk;
-	always #10 sys_rst = 1;
-	
-	always #20 tx_clk = !tx_clk;
-	always #1  tx_rst = 1;
-	
-	always #31 rx1_clk = !rx1_clk;
-	always #10 rx1_rst = 1;
-	
-	always #1.3 rx2_clk = !rx2_clk;
-	always #10  rx2_rst = 1;
-	
-	always @ (posedge sys_clk) begin
-		tx_prbs_en <= !tx_prbs_en;
-	end
-	
-	
+        .wr_data_count  (FIFO2_WR_CNT),
+        .rd_data_count  (FIFO2_RD_CNT)
+    );	
+    
+    logic                       RX1_BIT_CNT_FULL;
+    logic [BIT_CNT_WIDTH-1:0]   RX1_BIT_CNT;
+    logic [ERR_CNT_WIDTH-1:0]   RX1_ERR_CNT;
+    logic [9:0]                 RX1_SYNC_CNT;
+    logic                       RX1_DIN_VLD;
+    logic                       RX1_DIN;
+    logic                       RX1_DOUT_XOR;
+    
+    assign RX1_DIN_VLD = FIFO1_DOUT_VLD;
+    assign RX1_DIN = FIFO1_DOUT[7];
+    
+    PRBS_RX #(
+        .PRBS_TYPE      (PRBS_TYPE),
+        .BIT_CNT_WIDTH  (BIT_CNT_WIDTH),
+        .ERR_CNT_WIDTH  (ERR_CNT_WIDTH)
+    )_PRBS_RX1(
+        .clk            (rx1_clk),
+        .rst            (rx1_rst),
+        
+        // Input
+        .din_vld        (RX1_DIN_VLD),
+        .din            (RX1_DIN),
+    
+        // Output
+        .bit_cnt        (RX1_BIT_CNT),
+        .bit_cnt_full   (RX1_BIT_CNT_FULL),
+        .err_cnt        (RX1_ERR_CNT),
+        .dout_vld       (rx1_dout_vld),
+        .dout           (rx1_dout),
+        
+        .sync_cnt       (RX1_SYNC_CNT),
+        .dout_xor       (RX1_DOUT_XOR)
+    );
+    
+    logic                       RX2_BIT_CNT_FULL;
+    logic [BIT_CNT_WIDTH-1:0]   RX2_BIT_CNT;
+    logic [ERR_CNT_WIDTH-1:0]   RX2_ERR_CNT;
+    logic [9:0]                 RX2_SYNC_CNT;
+    logic                       RX2_DIN_VLD;
+    logic                       RX2_DIN;
+    logic                       RX2_DOUT_XOR;
+    
+    assign RX2_DIN_VLD = FIFO2_DOUT_VLD;
+    assign RX2_DIN = FIFO2_DOUT[7];
+    
+    PRBS_RX #(
+        .PRBS_TYPE      (PRBS_TYPE),
+        .BIT_CNT_WIDTH  (BIT_CNT_WIDTH),
+        .ERR_CNT_WIDTH  (ERR_CNT_WIDTH)
+    )_PRBS_RX2(
+        .clk            (rx2_clk),
+        .rst            (rx2_rst),
+        
+        // Inputs
+        .din_vld        (RX2_DIN_VLD),
+        .din            (RX2_DIN),
+    
+        // Outputs
+        .bit_cnt        (RX2_BIT_CNT),
+        .bit_cnt_full   (RX2_BIT_CNT_FULL),
+        .err_cnt        (RX2_ERR_CNT),
+        .dout_vld       (rx2_dout_vld),
+        .dout           (rx2_dout),
+        
+        .sync_cnt       (RX2_SYNC_CNT),
+        .dout_xor       (RX2_DOUT_XOR)
+    );
+    
+    always #20 sys_clk = !sys_clk;
+    always #10 sys_rst = 1;
+    
+    always #20 tx_clk = !tx_clk;
+    always #1  tx_rst = 1;
+    
+    always #31 rx1_clk = !rx1_clk;
+    always #10 rx1_rst = 1;
+    
+    always #1.3 rx2_clk = !rx2_clk;
+    always #10  rx2_rst = 1;
+    
+    always @ (posedge sys_clk)
+        tx_prbs_en <= !tx_prbs_en;
+    
 endmodule
--- src/PRBS_RX.v
+++ src/PRBS_RX.v
@@ -1,240 +1,182 @@
-/********************************************************************************/
-// Engineer:        
-// Design Name: PRBS_RX
-// Module Name: PRBS_RX     
-// Target Device:    
-// Description: PRBS receiver
-//      
-// Dependencies:
-//    None
-// Revision:
-//    None
-// Additional Comments: 
-//
-/********************************************************************************/
-
 `ifndef _PRBS_RX
 `define _PRBS_RX
 
 `timescale 1 ns/1 ps
 module PRBS_RX #(parameter 
-							PRBS_TYPE = 7,
-							BIT_CNT_WIDTH = 3,
-							ERR_CNT_WIDTH = 3
-	
+    PRBS_TYPE     = 7,
+    BIT_CNT_WIDTH = 3,
+    ERR_CNT_WIDTH = 3
+    
 )(
-	input 							clk,
-	input 							rst,
-	
-	// Input interface
-	input 							din_vld,
-	input 							din,
-	
-	// Output interface
-	output [BIT_CNT_WIDTH-1:0]		bit_cnt,
-	output 							bit_cnt_full,
-	output [ERR_CNT_WIDTH-1:0] 		err_cnt,
-	output 							dout_vld,
-	output 							dout,
-	
-	// test outputs
-	output [9:0]					sync_cnt,
-	output 							dout_xor
-	
+    input   clk,
+    input   rst,
+    
+// Inputs
+    input   din_vld,
+    input   din,
+    
+// Outputs
+    output [BIT_CNT_WIDTH-1:0]      bit_cnt,
+    output                          bit_cnt_full,
+    output [ERR_CNT_WIDTH-1:0]      err_cnt,
+    output                          dout_vld,
+    output                          dout,
+    
+// test outputs
+    output [9:0]    sync_cnt,
+    output          dout_xor
+    
 );
 
-/********************************************************************************/
-// Parameters section
-/********************************************************************************/ 
-/********************************************************************************/
-// Signals declaration section
-/********************************************************************************/
-	reg [BIT_CNT_WIDTH-1:0]			BIT_CNT_REG;
-	reg [9:0]						SYNC_CNT_REG;
-	
-	reg 							BIT_CNT_FULL_REG;
-	reg [ERR_CNT_WIDTH-1:0]			ERR_CNT_REG;	
-	
-	wire [2:0] 						PRBS_PARAM;
-	
-	assign PRBS_PARAM = PRBS_TYPE;
+//--------------------
+// Parameters & signals
+//--------------------
+    reg [BIT_CNT_WIDTH-1:0]			BIT_CNT_REG;
+    reg [9:0]						SYNC_CNT_REG;
+    
+    reg 							BIT_CNT_FULL_REG;
+    reg [ERR_CNT_WIDTH-1:0]			ERR_CNT_REG;	
+    
+    wire [2:0] 						PRBS_PARAM;
+    
+    assign PRBS_PARAM = PRBS_TYPE;
 
-/********************************************************************************/
-// Main section
-/********************************************************************************/	
-	/****************************************************************************/
-    // Delay
-    /****************************************************************************/  
-		reg 				DIN_VLD_REG;
-		reg 				DIN_REG;
-	
-		always @ (posedge clk or negedge rst) begin
-			if (!rst) begin
-				DIN_VLD_REG  <= 1'b0;
-				DIN_REG		<= 1'b0;
-			end
-			else begin
-				DIN_VLD_REG  <= din_vld;
-				DIN_REG		<= din;
-			end
-		end
-	
-	/****************************************************************************/
-    // Input bit counter
-    /****************************************************************************/ 
-		reg [31:0] 			DIN_CNT;
-		reg [31:0]			DIN_CNT_COEF;
-		
-		always @ (*) begin
-			case (PRBS_PARAM)
-				3'd0: DIN_CNT_COEF = 32'd7; 
-				3'd1: DIN_CNT_COEF = 32'd127;	
-				3'd2: DIN_CNT_COEF = 32'd511;
-				3'd3: DIN_CNT_COEF = 32'd2047; 	
-				3'd4: DIN_CNT_COEF = 32'd32767; 
-				3'd5: DIN_CNT_COEF = 32'd131071; 
-				3'd6: DIN_CNT_COEF = 32'd8388607; 
-				3'd7: DIN_CNT_COEF = 32'd4294967295; 
-			endcase
-		end
-		
-		always @ (posedge clk or negedge rst) begin
-			if (!rst) begin
-				DIN_CNT <= 32'h0000_0000;
-			end
-			else if (DIN_CNT == DIN_CNT_COEF) begin
-				DIN_CNT <= 32'h0000_0001;
-			end
-			else if (DIN_VLD_REG) begin
-				DIN_CNT <= DIN_CNT + 1'b1;
-			end
-		end 
-	
-	/****************************************************************************/
-    // Shift register
-    /****************************************************************************/ 
-		reg [31:0] 			DIN_SHIFT_REG;
-		reg [31:0]			DIN_FRAME_REG;
-		reg 				SYNC_FLAG;
-		reg					XOR_OUT;
-	
-		always @ (posedge clk or negedge rst) begin
-			if (!rst) begin
-				DIN_SHIFT_REG <= 32'h0000_0000;
-			end
-			else if (SYNC_FLAG && DIN_VLD_REG) begin
-				DIN_SHIFT_REG[0] <= XOR_OUT;
-				DIN_SHIFT_REG[31:1] <= DIN_SHIFT_REG[30:0];
-			end
-			else if (DIN_VLD_REG) begin
-				DIN_SHIFT_REG[0] <= DIN_REG;
-				DIN_SHIFT_REG[31:1] <= DIN_SHIFT_REG[30:0];
-			end
-		end
-		
-		always @ (*) begin
-			case (PRBS_PARAM)
-				3'd0: 	 XOR_OUT = DIN_SHIFT_REG[2]  ^ DIN_SHIFT_REG[0]; 
-				3'd1: 	 XOR_OUT = DIN_SHIFT_REG[6]  ^ DIN_SHIFT_REG[0];  
-				3'd2: 	 XOR_OUT = DIN_SHIFT_REG[8]  ^ DIN_SHIFT_REG[4];  
-				3'd3: 	 XOR_OUT = DIN_SHIFT_REG[10] ^ DIN_SHIFT_REG[8];  
-				3'd4: 	 XOR_OUT = DIN_SHIFT_REG[14] ^ DIN_SHIFT_REG[0];  
-				3'd5: 	 XOR_OUT = DIN_SHIFT_REG[16] ^ DIN_SHIFT_REG[2];  
-				3'd6:	 XOR_OUT = DIN_SHIFT_REG[22] ^ DIN_SHIFT_REG[17];
-				3'd7:	 XOR_OUT = DIN_SHIFT_REG[31] ^ DIN_SHIFT_REG[21] ^ DIN_SHIFT_REG[1] ^ DIN_SHIFT_REG[0];
-				default: XOR_OUT = 0;
-			endcase
-		end
-		
-	/****************************************************************************/
-    // Sync counter
-    /****************************************************************************/	
-		always @ (posedge clk or negedge rst) begin
-			if (!rst) begin 
-				SYNC_CNT_REG <= 0;
-			end
-			else if (DIN_VLD_REG) begin
-				if (DIN_REG == XOR_OUT) begin
-					SYNC_CNT_REG <= SYNC_CNT_REG + 1'b1;
-				end
-				else begin 
-					SYNC_CNT_REG <= 0;
-				end
-			end
-		end
-	
-		always @ (posedge clk or negedge rst) begin
-			if (!rst) begin 
-				SYNC_FLAG <= 0;
-			end
-			else if (SYNC_CNT_REG == 100) begin
-				SYNC_FLAG <= 1'b1;
-			end
-		end
-	
-	/****************************************************************************/
-    // Error/data counters
-    /****************************************************************************/ 	
-		always @ (posedge clk or negedge rst) begin
-			if (!rst) begin
-				BIT_CNT_REG <= 32'h0000_0000;
-			end
-			else if (DIN_VLD_REG) begin
-				BIT_CNT_REG <= BIT_CNT_REG + 1'b1;
-			end
-		end
-	
-		always @ (posedge clk or negedge rst) begin
-			if (!rst) begin
-				BIT_CNT_FULL_REG <= 1'b0;
-			end
-			else if (BIT_CNT_REG == 2**BIT_CNT_WIDTH-1) begin
-				BIT_CNT_FULL_REG <= 1'b1;
-			end
-			else begin
-				BIT_CNT_FULL_REG <= 1'b0;
-			end
-		end
-		
-	// Error counter	
-		wire 			ERR_EN;
-		assign ERR_EN = (DIN_REG ^ XOR_OUT) ? 1 : 0; 
-		
-		always @ (posedge clk or negedge rst) begin
-			if (!rst) begin
-				ERR_CNT_REG <= 32'h0000_0000;
-			end
-			else if (ERR_EN && DIN_VLD_REG) begin
-				ERR_CNT_REG <= ERR_CNT_REG + 1'b1;
-			end
-			else if (BIT_CNT_FULL_REG) begin
-				ERR_CNT_REG <= 32'h0000_0000;
-			end
-		end
-	
-	reg 							DOUT_VLD_REG;
-	
-	always @ (posedge clk or negedge rst) begin
-		if (!rst) begin 
-			DOUT_VLD_REG <= 1'b0;
-		end 
-		else begin
-			DOUT_VLD_REG <= din_vld;
-		end
-	end
-	
-/********************************************************************************/
+//--------------------
+// Main
+//--------------------
+
+// Delay
+    reg DIN_VLD_REG;
+    reg DIN_REG;
+    
+    always @ (posedge clk or negedge rst)
+        if (!rst) begin
+            DIN_VLD_REG  <= 1'b0;
+            DIN_REG		<= 1'b0;
+        end
+        else begin
+            DIN_VLD_REG  <= din_vld;
+            DIN_REG		<= din;
+        end
+
+// Input bit counter
+    reg [31:0] DIN_CNT;
+    reg [31:0] DIN_CNT_COEF;
+        
+    always @ (*)
+        case (PRBS_PARAM)
+            3'd0: DIN_CNT_COEF = 32'd7; 
+            3'd1: DIN_CNT_COEF = 32'd127;	
+            3'd2: DIN_CNT_COEF = 32'd511;
+            3'd3: DIN_CNT_COEF = 32'd2047; 	
+            3'd4: DIN_CNT_COEF = 32'd32767; 
+            3'd5: DIN_CNT_COEF = 32'd131071; 
+            3'd6: DIN_CNT_COEF = 32'd8388607; 
+            3'd7: DIN_CNT_COEF = 32'd4294967295; 
+        endcase
+        
+    always @ (posedge clk or negedge rst)
+        if (!rst)
+            DIN_CNT <= 32'h0000_0000;
+        else if (DIN_CNT == DIN_CNT_COEF)
+            DIN_CNT <= 32'h0000_0001;
+        else if (DIN_VLD_REG)
+            DIN_CNT <= DIN_CNT + 1'b1;
+    
+// Shift register
+    reg [31:0]  DIN_SHIFT_REG;
+    reg [31:0]  DIN_FRAME_REG;
+    reg         SYNC_FLAG;
+    reg	        XOR_OUT;
+    
+    always @ (posedge clk or negedge rst)
+        if (!rst) begin
+            DIN_SHIFT_REG <= 32'h0000_0000;
+        end else if (SYNC_FLAG && DIN_VLD_REG) begin
+                DIN_SHIFT_REG[0] <= XOR_OUT;
+                DIN_SHIFT_REG[31:1] <= DIN_SHIFT_REG[30:0];
+        end else if (DIN_VLD_REG) begin
+                DIN_SHIFT_REG[0] <= DIN_REG;
+                DIN_SHIFT_REG[31:1] <= DIN_SHIFT_REG[30:0];
+        end
+        
+    always @ (*)
+        case (PRBS_PARAM)
+            3'd0: XOR_OUT = DIN_SHIFT_REG[2]  ^ DIN_SHIFT_REG[0]; 
+            3'd1: XOR_OUT = DIN_SHIFT_REG[6]  ^ DIN_SHIFT_REG[0];  
+            3'd2: XOR_OUT = DIN_SHIFT_REG[8]  ^ DIN_SHIFT_REG[4];  
+            3'd3: XOR_OUT = DIN_SHIFT_REG[10] ^ DIN_SHIFT_REG[8];  
+            3'd4: XOR_OUT = DIN_SHIFT_REG[14] ^ DIN_SHIFT_REG[0];  
+            3'd5: XOR_OUT = DIN_SHIFT_REG[16] ^ DIN_SHIFT_REG[2];  
+            3'd6: XOR_OUT = DIN_SHIFT_REG[22] ^ DIN_SHIFT_REG[17];
+            3'd7: XOR_OUT = DIN_SHIFT_REG[31] ^ DIN_SHIFT_REG[21] ^ DIN_SHIFT_REG[1] ^ DIN_SHIFT_REG[0];
+            default: XOR_OUT = 0;
+        endcase
+        
+// Sync counter
+    always @ (posedge clk or negedge rst)
+        if (!rst) begin 
+            SYNC_CNT_REG <= 0;
+        end else if (DIN_VLD_REG) begin
+            if (DIN_REG == XOR_OUT)
+                SYNC_CNT_REG <= SYNC_CNT_REG + 1'b1;
+            else
+                SYNC_CNT_REG <= 0;
+        end
+    
+    always @ (posedge clk or negedge rst)
+        if (!rst)
+            SYNC_FLAG <= 0;
+        else if (SYNC_CNT_REG == 100)
+            SYNC_FLAG <= 1'b1;
+    
+// Error/data counters
+    always @ (posedge clk or negedge rst)
+        if (!rst)
+            BIT_CNT_REG <= 32'h0000_0000;
+        else if (DIN_VLD_REG)
+            BIT_CNT_REG <= BIT_CNT_REG + 1'b1;
+
+    always @ (posedge clk or negedge rst)
+        if (!rst)
+            BIT_CNT_FULL_REG <= 1'b0;
+        else if (BIT_CNT_REG == 2**BIT_CNT_WIDTH-1)
+            BIT_CNT_FULL_REG <= 1'b1;
+        else
+            BIT_CNT_FULL_REG <= 1'b0;
+        
+// Error counter	
+    wire ERR_EN;
+    assign ERR_EN = (DIN_REG ^ XOR_OUT) ? 1 : 0; 
+        
+    always @ (posedge clk or negedge rst)
+        if (!rst)
+            ERR_CNT_REG <= 32'h0000_0000;
+        else if (ERR_EN && DIN_VLD_REG)
+            ERR_CNT_REG <= ERR_CNT_REG + 1'b1;
+        else if (BIT_CNT_FULL_REG)
+            ERR_CNT_REG <= 32'h0000_0000;
+    
+    reg DOUT_VLD_REG;
+    
+    always @ (posedge clk or negedge rst)
+        if (!rst)
+            DOUT_VLD_REG <= 1'b0;
+        else
+            DOUT_VLD_REG <= din_vld;
+    
+//--------------------
 // Output
-/********************************************************************************/
-	assign dout = SYNC_FLAG ? XOR_OUT : 1'b0; //GEN_DOUT : 1'b0;
-	assign dout_vld = SYNC_FLAG ? DOUT_VLD_REG : 1'b0;
-	assign bit_cnt = BIT_CNT_REG;
-	assign bit_cnt_full = BIT_CNT_FULL_REG;
-	assign err_cnt = ERR_CNT_REG;
-	
-	assign dout_xor = XOR_OUT;
-	assign sync_cnt = SYNC_CNT_REG;
-	
+//--------------------
+    assign dout = SYNC_FLAG ? XOR_OUT : 1'b0; //GEN_DOUT : 1'b0;
+    assign dout_vld = SYNC_FLAG ? DOUT_VLD_REG : 1'b0;
+    assign bit_cnt = BIT_CNT_REG;
+    assign bit_cnt_full = BIT_CNT_FULL_REG;
+    assign err_cnt = ERR_CNT_REG;
+    
+    assign dout_xor = XOR_OUT;
+    assign sync_cnt = SYNC_CNT_REG;
+    
 endmodule
 
 `endif // _PRBS_RX
